3ed831c8e3f49487a9152774c9ac15d57e59772d
ETANTO NUARI TIDAR <etantonuaritidar@ETANTOs-MacBook-Pro.local>
2020-12-04 19:49:35 +0700

    Submission 2

---
+++ EtantoSubmission2/app/src/main/java/com/example/etanto_submission2/UserDetail.kt
@@ -0,0 +1,69 @@
+package com.example.etanto_submission2
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.user_detail.*
+
+class UserDetail : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_DATA = "extra_data"
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_detail)
+        setData()
+        viewPagerConfig()
+    }
+
+    private fun viewPagerConfig() {
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        view_pager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(view_pager)
+
+        supportActionBar?.elevation = 0f
+    }
+
+
+    private fun setActionBarTitle(title: String) {
+        if (supportActionBar != null) {
+            this.title = title
+        }
+    }
+
+    @SuppressLint("SetTextI18n", "StringFormatInvalid")
+    private fun setData() {
+        val dataUser = intent.getParcelableExtra(EXTRA_DATA) as UserData
+        dataUser.name?.let { setActionBarTitle(it) }
+        detail_name.text = dataUser.name
+        detail_username.text = dataUser.username
+        detail_company.text = getString(R.string.company, dataUser.company)
+        detail_location.text = getString(R.string.location, dataUser.location)
+        detail_repository.text = getString(R.string.repository, dataUser.repository)
+        Glide.with(this)
+            .load(dataUser.avatar)
+            .into(detail_avatar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_change_settings) {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
